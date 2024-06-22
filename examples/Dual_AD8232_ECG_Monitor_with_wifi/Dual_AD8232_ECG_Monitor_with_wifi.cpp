1ad149bef665198b930ddda1f7b84c5a8a73167f
TheXSpacer <p.cardenasc@pucp.pe>
2024-06-22 16:19:52 -0500

    220624

--- examples/Dual_AD8232_ECG_Monitor_with_wifi/Dual_AD8232_ECG_Monitor_with_wifi.cpp
+++ examples/Dual_AD8232_ECG_Monitor_with_wifi/Dual_AD8232_ECG_Monitor_with_wifi.cpp
@@ -12,51 +12,33 @@
 #include <XSpaceBioV10.h>
 #include <XSpaceIoT.h>
 
-// Pin definitions for the first AD8232 sensor setup
-#define ECG_OUT_A 34  // ECG output pin for the first AD8232 sensor
-#define SDN_A 33      // Shutdown pin for the first AD8232 sensor
-#define LOH_A 25      // Lead-off detection high pin for the first sensor
-#define LOL_A 26      // Lead-off detection low pin for the first sensor
-#define AD8232_A 0    // Identifier for the first AD8232 sensor
-
-// Pin definitions for the second AD8232 sensor setup
-#define ECG_OUT_B 35  // ECG output pin for the second AD8232 sensor
-#define SDN_B 27      // Shutdown pin for the second AD8232 sensor
-#define LOH_B 14      // Lead-off detection high pin for the second sensor
-#define LOL_B 13      // Lead-off detection low pin for the second sensor
-#define AD8232_B 1    // Identifier for the second AD8232 sensor
-
 // Create an instance of the XSpaceBioV10Board to interact with the hardware board
 XSpaceBioV10Board Board;
 XSEthernet XSnet;
 
+// Variables for signal processing
+int MaxDataIndex = 400;
+double ECGData[MaxDataIndex];
+double ECGData_procesed[MaxDataIndex];
+double ECGData_time[MaxDataIndex];
+double bpm = 0;
+
+//Declaration por GetBPM() function
 double GetBPM();
 
 void setup() {
   // Initialize serial communication at 115200 bps for output to the computer
   Serial.begin(115200);
 
-  XSnet.Wifi_init("Delta","c9aa28ba93");
-  XSnet.UDP_Connect("192.168.31.150",55000); //You can use Aurora app from www.xspace.pe
-  
-  
-  // Initialize both AD8232 sensors with their specific pin configurations
-  Board.AD8232_init(AD8232_XS1, SDN_A, LOH_A, LOL_A, ECG_OUT_A);
-  Board.AD8232_init(AD8232_XS2, SDN_B, LOH_B, LOL_B, ECG_OUT_B);
+  Board.init();
 
+  XSnet.Wifi_init("Delta","c9aa28ba93");
+  XSnet.UDP_Connect("192.168.31.150",55000); //You can use Aurora App from www.xspace.pe
+  
   // Activate the AD8232 sensors to begin ECG data acquisition
   Board.AD8232_Wake(AD8232_XS1);
   Board.AD8232_Wake(AD8232_XS2);
 }
-
-
-
-
-double ECGData[400];
-double ECGData_procesed[400];
-double ECGData_time[400];
-int i = 0;
-double bpm = 0;
 
 void loop() {
 
@@ -70,7 +52,7 @@ void loop() {
   Serial.println((String)ECGData_procesed[i] + " " + (String)ECGData_procesed[i] + " " + (String)bpm);
   XSnet.println((String)ECGData_procesed[i] + " " + (String)ECGData_procesed[i] + " " + (String)bpm);
   
-  if(i==400){
+  if(i==MaxDataIndex){
     bpm =  GetBPM();
     i = 0;
   }
@@ -81,7 +63,7 @@ void loop() {
 
 double GetBPM(){
   double max,min,freq;
-  Board.SignalAnalizer(ECGData,ECGData_time,ECGData_procesed,400,&max,&min,&freq);
+  Board.SignalAnalizer(ECGData,ECGData_time,ECGData_procesed,MaxDataIndex,&max,&min,&freq);
 
   return freq*60.0;
 }
